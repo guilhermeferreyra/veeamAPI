dfa874b119ed68ca2d670e905d880064c4bcae7c
Guilherme Ferreira <barret.gui@hotmail.com>
2021-01-19 14:51:53 -0300

    Update api.php

--- api.php
+++ api.php
@@ -1,4 +1,23 @@
 <?php
-echo "teste";
+
+if(strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') != 0){
+    throw new Exception('Request method must be POST!');
+}
+ 
+
+$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
+if(strcasecmp($contentType, 'application/json') != 0){
+    throw new Exception('Content type must be: application/json');
+}
+ 
+$content = trim(file_get_contents("php://input"));
+
+
+$decoded = json_decode($content, true);
+ 
+if(!is_array($decoded)){
+    throw new Exception('Received content contained invalid JSON!');
+}
+var_dump($decoded);
 
 ?>
