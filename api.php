d9faf0059e23584a6521cc78e3096e62e819ca10
Guilherme Ferreira <barret.gui@hotmail.com>
2021-01-21 15:22:42 -0300

    Update api.php

--- api.php
+++ api.php
@@ -23,32 +23,72 @@ function conexaoPDO(){
     return $pdo;
 }
 
-function inserirEditora($nome, $site, $email, $telefone){ 
+$job_name = $decoded=>'Job Name';
+$customer = $decoded=>'Customer';
+$state = $decoded=>'State';
+$start_time = $decoded=>'Start_Time';
+$end_time = $decoded=>'Stop_Time';
+$duration = $decoded=>'Duration';
+$avg_speed = $decoded=>'Avg_Speed';
+$data_processed = $decoded=>'Processed';
+$data_total = $decoded=>'Customer';
+$data_read = $decoded=>'Data_Read';
+$data_transferred = $decoded=>'Transferred';
+$data_dedupe = $decoded=>'DedupeRate';
+$data_compress = $decoded=>'CompressionRate';
+
+inserirEditora($job_name, $customer, $state, $start_time, $end_time, $duration, $avg_speed, $data_processed, $data_total, $data_read, $data_transferred, $data_dedupe, $data_compress)
+
+function inserirEditora($job_name, $customer, $state, $start_time, $end_time, $duration, $avg_speed, $data_processed, $data_total, $data_read, $data_transferred, $data_dedupe, $data_compress){ 
     $pdo = conexaoPDO();
     $stmt = $pdo->prepare('
-        INSERT INTO Editora(
-            nome,
-            site,
-            email, 
-            telefone 
+        INSERT INTO veeam_api(
+            customer,
+            state,
+            start_time,
+            end_time,
+            duration,
+            avg_speed,
+            data_processed,
+            data_total,
+            data_read,
+            data_transferred,
+            data_dedupe,
+            data_compress,
+            job_name
         )VALUES(
-            :nome, 
-            :site,
-            :email, 
-            :telefone 
+            :customer,
+            :state,
+            :start_time,
+            :end_time,
+            :duration,
+            :avg_speed,
+            :data_processed,
+            :data_total,
+            :data_read,
+            :data_transferred,
+            :data_dedupe,
+            :data_compress,
+            :job_name
         );' 	
     );
 
-    $stmt->bindValue(":nome", $nome);
-    $stmt->bindValue(":site", $site);
-    $stmt->bindValue(":email", $email);
-    $stmt->bindValue(":telefone", $telefone);
+    $stmt->bindValue(":customer", $customer);
+    $$stmt->bindValue(":state", $state
+    $stmt->bindValue(":start_time", $start_time
+    $stmt->bindValue(":end_time", $end_time
+    $stmt->bindValue(":duration", $duration
+    $stmt->bindValue(":avg_speed", $avg_speed
+    $stmt->bindValue(":data_processed", $data_processed
+    $stmt->bindValue(":data_total", $data_total
+    $stmt->bindValue(":data_read", $data_read
+    $stmt->bindValue(":data_transferred", $data_transferred
+    $stmt->bindValue(":data_dedupe", $data_dedupe
+    $stmt->bindValue(":data_compress", $data_compress
+    $stmt->bindValue(":job_name", $job_name
+
     $stmt->execute();
     
     return $pdo->lastInsertId();
 }	
-
-
-
-
 ?>
