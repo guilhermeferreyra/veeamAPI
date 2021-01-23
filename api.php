58d99ff4be663ec0acd2178acb79173814b1e84a
Guilherme Ferreira <barret.gui@hotmail.com>
2021-01-23 20:10:00 -0300

    Commit

--- api.php
+++ api.php
@@ -12,64 +12,21 @@ $content = trim(file_get_contents("php://input"));
 $decoded = json_decode($content);
 
 
-$job_name = $decoded->JobName;
 $customer = $decoded->Customer;
-$last_status = $decoded->Result;
-$start_time = $decoded->Start_Time->DateTime;
-$end_time = $decoded->Stop_Time->DateTime;
+$job_name = $decoded->Job_Name;
+$last_status = $decoded->State;
+$start_time = $decoded->Start_Time;
+$end_time = $decoded->Stop_Time;
 $duration = $decoded->Duration;
 $avg_speed = $decoded->Avg_Speed;
 $data_processed = $decoded->Processed;
-$data_total = $decoded->Customer;
-$data_read = $decoded->Data_Read;
+$data_total = $decoded->Total;
+$data_read = $decoded->Data_read;
 $data_transferred = $decoded->Transferred;
 $data_dedupe = $decoded->DedupeRate;
 $data_compress = $decoded->CompressionRate;
 
-/*
-$insert = [
-    ':customer' => $customer,
-    ':last_status' => $last_status,
-    ':start_time' => $start_time,
-    ':end_time' => $end_time,
-    ':duration' => $duration,
-    ':avg_speed' => $avg_speed,
-    ':data_processed' => $data_processed,
-    ':data_total' => $data_total,
-    ':data_read' => $data_read,
-    ':data_transferred' => $data_transferred,
-    ':data_dedupe' => $data_dedupe,
-    ':data_compress' => $data_compress,
-    ':job_name' => $job_name,
-];
-
-
-{
-    "Job Name":  "INFINIIT (Incremental)",
-    "State":  -1,
-    "JobName":  "INFINIIT",
-    "Customer":  "Infiniit Dom Pedro",
-    "Start_Time":  {
-                       "value":  "\/Date(1611190823377)\/",
-                       "DateTime":  "quarta-feira, 20 de janeiro de 2021 22:00:23"
-                   },
-    "Stop_Time":  {
-                      "value":  "\/Date(1611191815627)\/",
-                      "DateTime":  "quarta-feira, 20 de janeiro de 2021 22:16:55"
-                  },
-    "Duration":  "0:16:32",
-    "Avg_Speed":  88.01,
-    "Total":  1120,
-    "Processed":  432.32,
-    "Data_Read":  8.9,
-    "Transferred":  2.35,
-    "DedupeRate":  "1.1x",
-    "CompressionRate":  "2.3x",
-    "Details":  "",
-    "Result":  0
-}
-
-*/
+echo $last_status."\n";
 
 try
 {
@@ -126,8 +83,6 @@ $stmt->bindParam(':data_dedupe', $data_dedupe);
 $stmt->bindParam(':data_compress', $data_compress);
 $stmt->bindParam(':job_name', $job_name);
 
-var_dump($stmt);
-
 $result = $stmt->execute();
  
 if ( ! $result )
@@ -137,42 +92,4 @@ if ( ! $result )
 }
  
 echo $stmt->rowCount() . "linhas inseridas";
-
-
-
-/*
-$sql = "INSERT INTO veeam_api (customer, last_status, start_time, end_time, duration, avg_speed, data_processed, data_total, data_read, data_transferred, data_dedupe, data_compress, job_name) 
-VALUES (:customer, :last_status, :start_time, :end_time, :duration, :avg_speed, :data_processed, :data_total, :data_read, :data_transferred, :data_dedupe, :data_compress, :job_name";
-$stmt= $pdo->prepare($sql);
-var_dump($pdo);
-$stmt->execute($insert);
-*/
-
-/*
-populateDb($job_name, $customer, $last_status, $start_time, $end_time, $duration, $avg_speed, $data_processed, $data_total, $data_read, $data_transferred, $data_dedupe, $data_compress);
-
-function populateDb($job_name, $customer, $last_status, $start_time, $end_time, $duration, $avg_speed, $data_processed, $data_total, $data_read, $data_transferred, $data_dedupe, $data_compress){ 
-    $pdo = conexaoPDO();
-    $stmt = $pdo->prepare('INSERT INTO veeam_api (customer,state,start_time,end_time,duration,avg_speed,data_processed,data_total,data_read,data_transferred,data_dedupe,data_compress,job_name) VALUES (:customer,:state,:start_time,:end_time,:duration,:avg_speed,:data_processed,:data_total,:data_read,:data_transferred,:data_dedupe,:data_compress,:job_name);' 	
-    );
-
-    $stmt->bindParam("customer", $customer);
-    $stmt->bindParam("state", $last_status);
-    $stmt->bindParam("start_time", $start_time);
-    $stmt->bindParam("end_time", $end_time);
-    $stmt->bindParam("duration", $duration);
-    $stmt->bindParam("avg_speed", $avg_speed);
-    $stmt->bindParam("data_processed", $data_processed);
-    $stmt->bindParam("data_total", $data_total);
-    $stmt->bindParam("data_read", $data_read);
-    $stmt->bindParam("data_transferred", $data_transferred);
-    $stmt->bindParam("data_dedupe", $data_dedupe);
-    $stmt->bindParam("data_compress", $data_compress);
-    $stmt->bindParam("job_name", $job_name);
-
-    $stmt->execute();
-    
-    return $pdo->lastInsertId();
-}
-*/
 ?>
--- getJobs.ps1
+++ getJobs.ps1
@@ -1,55 +1,47 @@
-Add-PSSnapin VeeamPSSnapIn
+#region Config
+$veeamServer = "denver"
+$veeamDeployment = "Infiniit"#+ $env:COMPUTERNAME
+$APIendpoint = "http://localhost/veeamAPI/api.php"
+$HourstoCheck = 720
+$credentials = "C:\Users\guilherme.ferreira\cred.txt"
+#endregion
 
-$veeamDeployment = "Infiniit Dom Pedro" # Vulgo Cliente
+Disconnect-VBRServer
+Connect-VBRServer -Server $veeamServer -Credential (Import-CliXml -Path $credentials) -ErrorAction Ignore
 
-#Connect-VBRServer -Server denver -Credential (Import-CliXml -Path 'PathToCred\cred.txt')
+#region Script
+$backupSessions = Get-VBRBackupSession
 
-$HourstoCheck = 72
+$backupSessions = @($backupSessions | Where-Object {($_.EndTime -ge (Get-Date).AddHours(-$HourstoCheck) -or $_.CreationTime -ge (Get-Date).AddHours(-$HourstoCheck) -or $_.State -eq "Working") -and $_.JobType -eq "Backup"})
 
-$allSess = Get-VBRBackupSession
-<#
-    $PreviousDay = [DateTime]::Now.AddHours(-$HourstoCheck)
-    $sessions = Get-VBRBackupSession | Where-Object {($_.EndTime -gt $PreviousDay -or $_.StartTime -gt $PreviousDay)}
-    foreach($session in $sessions){
-    $lastResult = Get-VBRJob | Where-Object {$_.Name -eq $session.JobName}
-    $lastResult = $lastResult.Info.LatestStatus
-    $sessionStatus = $session.Result
-    $startTime = $session.Progress.StartTimeLocal
-    $endTime = $session.Progress.StopTimeLocal
-    $duration = $session.Progress.Duration.TotalSeconds
-    $backupSize = $session.BackupStats.BackupSize
-  #>
-  $sessListBk = @($allSess | Where-Object {($_.EndTime -ge (Get-Date).AddHours(-$HourstoCheck) -or $_.CreationTime -ge (Get-Date).AddHours(-$HourstoCheck) -or $_.State -eq "Working") -and $_.JobType -eq "Backup"})
+foreach($session in $backupSessions){
   
-  $arrAllSessBk = $sessListBk | Sort-Object -Property Creationtime | Select-Object -Property @{Name="Job Name"; Expression = {$_.Name}},
-        @{Name="State"; Expression = {$_.State}},
-        @{Name="JobName"; Expression = {$_.OrigJobName}},
-        @{Name="Customer"; Expression = {$veeamDeployment}},
-        @{Name="Start_Time"; Expression = {$_.CreationTime}},
-        @{Name="Stop_Time"; Expression = {If ($_.EndTime -eq "1/1/1900 12:00:00 AM"){"-"} Else {$_.EndTime}}},
-        @{Name="Duration"; Expression = {Get-Duration -ts $_.Progress.Duration}},                    
-        @{Name="Avg_Speed"; Expression = {[Math]::Round($_.Progress.AvgSpeed/1MB,2)}},
-        @{Name="Total"; Expression = {[Math]::Round($_.Progress.ProcessedSize/1GB,2)}},
-        @{Name="Processed"; Expression = {[Math]::Round($_.Progress.ProcessedUsedSize/1GB,2)}},
-        @{Name="Data_Read"; Expression = {[Math]::Round($_.Progress.ReadSize/1GB,2)}},
-        @{Name="Transferred"; Expression = {[Math]::Round($_.Progress.TransferedSize/1GB,2)}},
-        @{Name="DedupeRate"; Expression = {
-          If ($_.Progress.ReadSize -eq 0) {0}
-          Else {([string][Math]::Round($_.BackupStats.GetDedupeX(),1)) +"x"}}},
-        @{Name="CompressionRate"; Expression = {
-          If ($_.Progress.ReadSize -eq 0) {0}
-          Else {([string][Math]::Round($_.BackupStats.GetCompressX(),1)) +"x"}}},
-        @{Name="Details"; Expression = {($_.GetDetails()).Replace("<br />","ZZbrZZ")}}, Result
-        
-             
-#}
-
-foreach($data in $arrAllSessBk){
-  Invoke-WebRequest -Uri http://localhost/veeamAPI/api.php -Method Post -Body ($data | ConvertTo-Json) -ContentType 'application/json'
+  if ($session.Progress.ReadSize -eq 0){$dedupe = 0} 
+  Else {$dedup = [string][Math]::Round($session.BackupStats.GetDedupeX(),1) +"x"}
+  
+  if ($session.Progress.ReadSize -eq 0){$compress = 0}
+  Else {$compress = [string][Math]::Round($session.BackupStats.GetCompressX(),1) +"x"}
+  
+  $objSession = New-Object -TypeName PSObject
+  $objSession | Add-Member -MemberType NoteProperty -Name Job_Name -Value $session.Name
+  $objSession | Add-Member -MemberType NoteProperty -Name Customer -Value $veeamDeployment
+  $objSession | Add-Member -MemberType NoteProperty -Name State -Value ([string]$session.Info.Result)
+  $objSession | Add-Member -MemberType NoteProperty -Name Start_Time -Value ([datetime]$session.CreationTime | Get-date -Format "yyyy-MM-dd HH:mm:ss")
+  $objSession | Add-Member -MemberType NoteProperty -Name Stop_Time -Value ([datetime]$session.EndTime | Get-date -Format "yyyy-MM-dd HH:mm:ss")
+  $objSession | Add-Member -MemberType NoteProperty -Name Duration -Value (Get-Duration -ts $session.Progress.Duration)
+  $objSession | Add-Member -MemberType NoteProperty -Name Avg_Speed -Value ([Math]::Round($session.Progress.AvgSpeed/1MB,2))
+  $objSession | Add-Member -MemberType NoteProperty -Name Total -Value ([Math]::Round($session.Progress.ProcessedSize/1GB,2))
+  $objSession | Add-Member -MemberType NoteProperty -Name Processed -Value ([Math]::Round($session.Progress.ProcessedUsedSize/1GB,2))
+  $objSession | Add-Member -MemberType NoteProperty -Name Data_read -Value ([Math]::Round($session.Progress.ReadSize/1GB,2))
+  $objSession | Add-Member -MemberType NoteProperty -Name Transferred -Value ([Math]::Round($session.Progress.TransferedSize/1GB,2))
+  $objSession | Add-Member -MemberType NoteProperty -Name DedupeRate -Value $dedupe
+  $objSession | Add-Member -MemberType NoteProperty -Name CompressionRate -Value $compress
+  
+  $post = Invoke-WebRequest -Uri $APIendpoint -Method Post -Body ($objSession | ConvertTo-Json) -ContentType 'application/json'
 }
+#endregion
 
-
-
+#region Functions
 function Get-Duration {
   param ($ts)
   $days = ""
@@ -58,33 +50,4 @@ function Get-Duration {
   }
   "{0}{1}:{2,2:D2}:{3,2:D2}" -f $days,$ts.Hours,$ts.Minutes,$ts.Seconds
 }
-<#
-    function Get-BackupSize {
-    param ($backups)
-    $outputObj = @()
-    Foreach ($backup in $backups) {
-    $backupSize = 0
-    $dataSize = 0
-    $files = $backup.GetAllStorages()
-    Foreach ($file in $Files) {
-      $backupSize += [math]::Round([long]$file.Stats.BackupSize/1GB, 2)
-      $dataSize += [math]::Round([long]$file.Stats.DataSize/1GB, 2)
-    }         
-    $repo = If ($($script:repoList | Where {$_.Id -eq $backup.RepositoryId}).Name) {
-              $($script:repoList | Where {$_.Id -eq $backup.RepositoryId}).Name
-            } Else {
-              $($script:repoListSo | Where {$_.Id -eq $backup.RepositoryId}).Name
-            }
-    $vbrMasterHash = @{
-      JobName = $backup.JobName
-      VMCount = $backup.VmCount
-      Repo = $repo
-      DataSize = $dataSize
-      BackupSize = $backupSize
-    }
-    $vbrMasterObj = New-Object -TypeName PSObject -Property $vbrMasterHash
-    $outputObj += $vbrMasterObj
-    }
-    $outputObj
-    }
-#>
+#endregion
