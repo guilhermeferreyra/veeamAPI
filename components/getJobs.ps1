57fab7a59b721fc4b0e3f33eb119278b8b7040b7
Guilherme Ferreira <barret.gui@hotmail.com>
2021-02-09 17:27:04 -0300

    Ajuste de estrutura

--- components/getJobs.ps1
+++ components/getJobs.ps1
@@ -8,12 +8,21 @@ $HourstoCheck = 720
 Add-PSSnapin VeeamPSSnapin
 #Disconnect-VBRServer
 #Connect-VBRServer -Server $veeamServer -Credential (Import-CliXml -Path $credentials) -ErrorAction Ignore
-
 #endregion
+
+#region Functions
+function Get-Duration {
+  param ($ts)
+  $days = ""
+  If ($ts.Days -gt 0) {
+    $days = "{0}:" -f $ts.Days
+  }
+  "{0}{1}:{2,2:D2}:{3,2:D2}" -f $days,$ts.Hours,$ts.Minutes,$ts.Seconds
+}
+#endregion
+
 #region Script
-
 $agentJobs = Get-VBRComputerBackupJob
-
 foreach($agentJob in $agentJobs){
   $objAgentJob = New-Object -TypeName PSObject
   $objAgentJob | Add-Member -MemberType NoteProperty -Name Customer -Value $veeamDeployment
@@ -28,7 +37,6 @@ foreach($agentJob in $agentJobs){
 
 $agentSessions = Get-VBRComputerBackupJobSession
 $agentSessions = @($agentSessions | Where-Object {($_.CreationTime -ge (Get-Date).AddHours(-$HourstoCheck)) -and $_.State -eq "Stopped"})
-
 foreach($agentSession in $agentSessions){
 
   $objAgent = New-Object -TypeName PSObject
@@ -42,9 +50,22 @@ foreach($agentSession in $agentSessions){
   Invoke-WebRequest -Uri ($APIendpoint+"/sendAgent.php") -Method Post -Body ($objAgent | ConvertTo-Json) -ContentType 'application/json'
 }
 
+$backupJobs = Get-VBRJob
+foreach($backupJob in $backupJobs){
+  $objJob = New-Object -TypeName PSObject
+  $objJob | Add-Member -MemberType NoteProperty -Name JobName -Value $backupJob.Name
+  $objJob | Add-Member -MemberType NoteProperty -Name JobType -Value $backupJob.TypeToString
+  $objJob | Add-Member -MemberType NoteProperty -Name Uid -Value $backupJob.Uid.Uid
+  $objJob | Add-Member -MemberType NoteProperty -Name LatestRunLocal -Value ([datetime]$backupJob.CreationTime | Get-date -Format "yyyy-MM-dd HH:mm:ss")
+  $objJob | Add-Member -MemberType NoteProperty -Name LatestStatus -Value ([string]$backupJob.Info.LatestStatus)
+  $objJob | Add-Member -MemberType NoteProperty -Name Customer -Value $veeamDeployment
+  $objJob | Add-Member -MemberType NoteProperty -Name JobHash -Value ([string]$objJob.Uid + [string]$objJob.LatestRunLocal)
+
+  Invoke-WebRequest -Uri ($APIendpoint+"/sendJob.php") -Method Post -Body ($objJob | ConvertTo-Json) -ContentType 'application/json'
+}
+
 $backupSessions = Get-VBRBackupSession
 $backupSessions = @($backupSessions | Where-Object {($_.EndTime -ge (Get-Date).AddHours(-$HourstoCheck) -or $_.CreationTime -ge (Get-Date).AddHours(-$HourstoCheck) -or $_.State -eq "Working") <#-and $_.JobType -eq "Backup"#>})
-
 foreach($session in $backupSessions){
   
   if ($session.Progress.ReadSize -eq 0){$dedupe = 0} 
@@ -72,35 +93,11 @@ foreach($session in $backupSessions){
   $objSession | Add-Member -MemberType NoteProperty -Name CompressionRate -Value $compress
     
   Invoke-WebRequest -Uri ($APIendpoint+"/sendSession.php") -Method Post -Body ($objSession | ConvertTo-Json) -ContentType 'application/json' 
-  #$objSession | ConvertTo-Json
 }
 
-$backupJobs = Get-VBRJob
-#$backupJobs = @($backupJobs | Where-Object -Property TypeToString -NotLike "*Copy")
 
-foreach($backupJob in $backupJobs){
-  $objJob = New-Object -TypeName PSObject
-  $objJob | Add-Member -MemberType NoteProperty -Name JobName -Value $backupJob.Name
-  $objJob | Add-Member -MemberType NoteProperty -Name JobType -Value $backupJob.TypeToString
-  $objJob | Add-Member -MemberType NoteProperty -Name Uid -Value $backupJob.Uid.Uid
-  $objJob | Add-Member -MemberType NoteProperty -Name LatestRunLocal -Value ([datetime]$backupJob.CreationTime | Get-date -Format "yyyy-MM-dd HH:mm:ss")
-  $objJob | Add-Member -MemberType NoteProperty -Name LatestStatus -Value ([string]$backupJob.Info.LatestStatus)
-  $objJob | Add-Member -MemberType NoteProperty -Name Customer -Value $veeamDeployment
-  $objJob | Add-Member -MemberType NoteProperty -Name JobHash -Value ([string]$objJob.Uid + [string]$objJob.LatestRunLocal)
-
-  Invoke-WebRequest -Uri ($APIendpoint+"/sendJob.php") -Method Post -Body ($objJob | ConvertTo-Json) -ContentType 'application/json'
-}
 #endregion
-#region Functions
-function Get-Duration {
-  param ($ts)
-  $days = ""
-  If ($ts.Days -gt 0) {
-    $days = "{0}:" -f $ts.Days
-  }
-  "{0}{1}:{2,2:D2}:{3,2:D2}" -f $days,$ts.Hours,$ts.Minutes,$ts.Seconds
-}
-#endregion
+
 
 
 #Disconnect-VBRServer
